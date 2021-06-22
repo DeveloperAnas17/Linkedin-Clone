ba190d3fc6012bab3572013be87be84c3ff1ea1e
Anas <amanabbasi3660@gmail.com>
2021-06-22 12:21:29 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body"></div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
